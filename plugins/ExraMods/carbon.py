eef71e3e77b69b37d05877927f5f8eba5d01a756
WaldyNishi2 <138442977+WaldyNishi2@users.noreply.github.com>
2023-08-22 08:37:40 +0530

    Update carbon.py

--- plugins/ExraMods/carbon.py
+++ plugins/ExraMods/carbon.py
@@ -43,7 +43,7 @@ async def carbon_func(_, message):
     await message.reply_photo(
         photo=carbon,
         caption="**Made by @mkn_bots_updates**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/mkn_bots_updates")]]),                   
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/waldy_Writings")]]),                   
     )
     await m.delete()
     carbon.close()
